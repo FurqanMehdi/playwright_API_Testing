46edf4eff73fb42eb3e0778c918e3fdf466c4c8e
FurqanMehdi <furqanmehdi831@gmail.com>
2026-07-06 18:03:30 +0500

    GET api for practice

---
+++ tests/get.spec.js
@@ -0,0 +1,15 @@
+// tests/api.spec.js
+import { test, expect } from '@playwright/test';
+
+test('GET users API', async ({ request }) => {
+  const response = await request.get(
+    'https://jsonplaceholder.typicode.com/users'
+  );
+
+  expect(response.status()).toBe(200);
+
+  const body = await response.json();
+
+  expect(body.length).toBeGreaterThan(0);
+  expect(body[0]).toHaveProperty('id');
+});
