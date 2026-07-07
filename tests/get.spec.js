4dc62e97efc5eeb08ca819eb8af76c22f89da5ec
FurqanMehdi <furqanmehdi831@gmail.com>
2026-07-07 12:56:04 +0500

    Complete get Function

--- tests/get.spec.js
+++ tests/get.spec.js
@@ -1,19 +1,67 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('GET Users', async ({ request }) => {
-
+// -------------------------------------------
+//             GET ALL POSTS
+//-------------------------------------------
+test("Get all posts", async ({ request }) => {
   const response = await request.get(
-    'https://reqres.in/api/users?page=2',
-    {
-      headers: {
-        'x-api-key': 'free_user_3G8GCs4M0p1joNx6k8vJZi87Y4L'
-      }
-    }
+    "https://jsonplaceholder.typicode.com/posts",
   );
 
   expect(response.status()).toBe(200);
 
   const body = await response.json();
-  console.log(body);
+
+  expect(body.length).toBe(100);
+
+  expect(body[0]).toHaveProperty("id");
 });
 
+// -------------------------------------------
+//             GET SINGLE POST
+//------------------------------------------- 
+test("Get Single Post", async ({ request }) => {
+  const response = await request.get(
+    "https://jsonplaceholder.typicode.com/posts/1",
+  );
+  expect(response.status()).toBe(200);
+  const body = await response.json();
+
+  expect(body.id).toBe(1);
+  expect(body.userId).toBe(1);
+  expect(body.title).toBeTruthy();
+});
+
+
+//-------------------------------------------
+//             GET COMMENTS OF POST
+//-------------------------------------------
+test('Get comments of post', async({request}) => {
+   const response = await request.get(
+   'https://jsonplaceholder.typicode.com/posts/1/comments'
+   );
+   expect(response.status()).toBe(200);
+   
+   const body = await response.json();
+   expect(body.length).toBeGreaterThan(0);
+
+
+   body.forEach(comment => {
+     expect(comment.postId).toBe(1)
+   });
+});
+//-------------------------------------------
+//     GET COMMENTS USING QUERY PARAMETER
+//-------------------------------------------
+test('Get comments using query paramete', async({request}) => {
+  const response = await request.get(
+   'https://jsonplaceholder.typicode.com/comments?postId=1'
+  );
+  expect(response.ok()).toBeTruthy();
+   
+  const body = await response.json();
+
+   body.forEach(comment => {
+    expect(comment.postId).toBe(1);
+  });
+});
